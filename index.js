0be7d3edc652933d40adfdbfb17f02f32b55c46f
Seu Martins <Baielsantana@gmail.com>
2026-06-03 11:28:06 -0300

    gerenciamento de alunos CASA

--- index.js
+++ index.js
@@ -63,39 +63,62 @@
 
 class Aluno {
     constructor(nome, nota){
-        this.nome = nome;
-        this.nota = nota
+        this._nome = nome;
+        this._nota = nota
     }
     estaAprovado(){
-        let aprovado = this.nota >= 7;
+        let aprovado = this._nota >= 7;
         return aprovado
     }
 }
 
 class Turma {
-    constructor(listaDeAlunos) {
+    constructor(listaDeAlunos = []) {
         this.Alunos = listaDeAlunos;
     }
 
     gerarRelatorio(){
         return this.Alunos.filter((Aluno) => Aluno.estaAprovado())
     }
-}
-let listaAluno = []
 
-let Alunos = [
+    adicionarAluno(Aluno){
+        if(typeof Aluno.estaAprovado !== "function"){
+            throw new Error("o objeto nao tem o metodo necessario")
+        }
+        let addAluno = new Aluno
+        this.Aluno.push(addAluno)
+    }
+
+    adicionarAlunoEmMassa(Aluno){
+        alunosEmMassa = new Aluno
+        listaAluno.forEach(Aluno => {
+        this.adicionarAluno.push(Aluno)  
+        });
+
+        
+    }
+}
+    
+let listaAluno = [
   { nome: "Flavio", nota: 3.5 },
   { nome: "Patrick",  nota: 5.2 },
   { nome: "Lucas",  nota: 7.0 },
   { nome: "Robson",  nota: 6.9 },
   { nome: "Mario", nota: 9.3 }
-];
+]
+
+let novaListaAlunos = []
+listaAluno.forEach((Aluno) => {
+    let novoAluno = Aluno(Aluno.nome, Aluno.nota)
+    alunosInstanciados.push(addAluno)
+})
 
 
-for (let i = 0; i < Alunos.length; i++) {
-    let novoAluno = new Aluno(Alunos[i].nome, Alunos[i].nota)
-    listaAluno.push(novoAluno);
-}
 
-const turmaB = new Turma(listaAluno);
-console.log("Alunos Aprovados:", turmaB.gerarRelatorio());;
+let addAluno = new Aluno ("vitor", 7)
+let cajuhub = new Turma()
+cajuhub.adicionarAlunoEmMassa(Aluno)
+cajuhub.adicionarAluno(addAluno)
+console.log(cajuhub)
+
+
